56a348219221a41ef7ace6634beccd9aacb1aec0
axelsot0 <axelmlsoto@gmail.com>
2024-08-13 00:40:26 -0400

    Cambio entidades Producto/Reseña

---
+++ Entidades/Compra.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Compra
+    {
+        public int IdCompra { get; set; }
+
+        public int IdProducto { get; set; } 
+
+        public int IdCarrito { get; set;}
+    }
+}
---
+++ Entidades/Producto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Marca { get; set; }
+        public string Tipo { get; set; }
+        public string Modelo { get; set;}
+        public int Existencias { get; set; }
+        public string Descripcion { get; set; }
+        public  string Foto { get; set; }
+        public float Precio { get; set; }
+        public int IdReseña { get; set; }
+        public Reseña Reseña { get; set; }
+
+
+    }
+}
