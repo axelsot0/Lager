b56617957b49f48412722fb6f396ef65097ad006
axelsot0 <axelmlsoto@gmail.com>
2024-08-13 00:40:26 -0400

    Cambio entidad nueva Producto

---
+++ Entidades/Producto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Marca { get; set; }
+        public string Tipo { get; set; }
+        public string Modelo { get; set;}
+        public int Existencias { get; set; }
+        public string Descripcion { get; set; }
+        public  string Foto { get; set; }
+        public float Precio { get; set; }
+        public float Reseña { get; set; }
+
+
+    }
+}
