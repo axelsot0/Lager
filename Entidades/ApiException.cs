6fe8a66ca462e58c25698c85d660dfa64dae6237
Yahi-Dev <yahinnieltheking01@gmail.com>
2024-08-15 11:21:36 -0500

    Agregando el ApiException

---
+++ Entidades/ApiException.cs
@@ -0,0 +1,21 @@
+using System.Globalization;
+
+namespace Entidades
+{
+    public class ApiException : Exception
+    {
+        public int ErrorCode { get; set; }
+
+        public ApiException() : base() { }
+
+        public ApiException(string message) : base(message) { }
+
+        public ApiException(string message, int errorCode) : base(message)
+        {
+            ErrorCode = errorCode;
+        }
+
+        public ApiException(string message, params object[] args)
+        : base(String.Format(CultureInfo.CurrentCulture, message, args)) { }
+    }
+}
